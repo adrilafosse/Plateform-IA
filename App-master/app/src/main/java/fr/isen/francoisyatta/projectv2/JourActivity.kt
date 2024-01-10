9fd53c80a8d71d750d900e15c0d0cb241223cc15
solenedfn <solenedufournaud2112@gmail.com>
2024-01-10 16:21:56 +0100

    add jourActivity

---
+++ App-master/app/src/main/java/fr/isen/francoisyatta/projectv2/JourActivity.kt
@@ -0,0 +1,19 @@
+package fr.isen.francoisyatta.projectv2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.isen.francoisyatta.projectv2.fragment.Jour
+
+class JourActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jour)
+
+        val fragmentJour = Jour()
+
+        // Add the fragment to the activity
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragmentJour)
+            .commit()
+    }
+}
